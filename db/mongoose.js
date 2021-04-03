468a98e5d50d2f5310e48268ba940a0d816a0fbc
ChrisHolley <asiholley@gmail.com>
2021-04-03 15:37:24 -0700

    init express & mongoose

---
+++ db/mongoose.js
@@ -0,0 +1,41 @@
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/events', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('mongoose connected')
+
+});
+
+const eventSchema = new mongoose.Schema(
+  {
+    "trip_id": Number,
+    "event_id": Number,
+    "latitude": Number,
+    "longitude": Number,
+    "photos": [String],
+    "start_time": String,
+    "end_time": String,
+    "description": String,
+    "start_date": String,
+    "end_date": String,
+    "cost": Number,
+    "transportation": String,
+    "mandatory": Boolean,
+    "attending": [{ "student_id": Number, "notes": String}],
+    "important_info": {
+                        "embassy_phone": Number,
+                        "embassy_location_latitude": Number,
+                        "embassy_location_longitude": Number,
+                        "popo_phone": Number,
+                        "popo_location_latitude": Number,
+                        "popo_location_longitude": Number,
+                        "hospital_location_latitude": Number,
+                        "hospital_location_longitude": Number,
+    }
+  }
+);
+
+module.exports = mongoose.model('Event', eventSchema);
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const mdb = require('../db/mongoose.js')
+const app = express()
+const port = 3000
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
