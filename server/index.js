3f9cc5726813250b5a5e129cf07fa6ecda412646
ChrisHolley <asiholley@gmail.com>
2021-04-08 19:51:38 -0700

    update event dummy data photo urls

--- server/index.js
+++ server/index.js
@@ -122,7 +122,7 @@ app.post('/api/events', (req, res) => {
     "latitude": req.body.latitude,
     "longitude": req.body.longitude,
     // "photos": req.body.photos,
-    "photos": "https://unsplash.com/photos/iUBgeNeyVy8", //stock photo for now
+    "photos": "https://images.unsplash.com/photo-1527631746610-bca00a040d60?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80", //stock photo for now
     "start_time": req.body.start_time,
     "end_time": req.body.end_time,
     "description": req.body.description,
